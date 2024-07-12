b9fa265a331655bcfb8572cb12b34bdf269a14e8
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-07-12 00:09:16 -0300

    cryptoapp[Seção3: Projeto Crypto API] - Obtendo a carteira de criptomoedas,  implementando DTO pra transfêrencia de dados, Obtendo transações da carteira

--- java/curso-spring-boot/cryptoapp/src/main/java/com/mn/cryptoapp/controller/CoinController.java
+++ java/curso-spring-boot/cryptoapp/src/main/java/com/mn/cryptoapp/controller/CoinController.java
@@ -1,16 +1,15 @@
 package com.mn.cryptoapp.controller;
 
+import com.mn.cryptoapp.dto.CoinDTO;
 import com.mn.cryptoapp.entity.Coin;
 import com.mn.cryptoapp.repository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @RestController
 @RequestMapping("/coin")
@@ -19,12 +18,31 @@ public class CoinController {
     @Autowired
     private CoinRepository coinRepository;
 
+    @GetMapping()
+    public ResponseEntity get() {
+        try {
+            List<CoinDTO> coins = coinRepository.getAll();
+            return new ResponseEntity<>(coins, HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity get(@PathVariable String name) {
+        try {
+            return new ResponseEntity<>(coinRepository.getByName(name), HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @PostMapping()
     public ResponseEntity post(@RequestBody Coin coin) {
         try {
             coin.setDateTime(new Timestamp(System.currentTimeMillis()));
             return new ResponseEntity<>(coinRepository.insert(coin), HttpStatus.CREATED);
-        } catch (Exception error){
+        } catch (Exception error) {
             return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- java/curso-spring-boot/cryptoapp/src/main/java/com/mn/cryptoapp/repository/CoinRepository.java
+++ java/curso-spring-boot/cryptoapp/src/main/java/com/mn/cryptoapp/repository/CoinRepository.java
@@ -1,9 +1,15 @@
 package com.mn.cryptoapp.repository;
 
+import com.mn.cryptoapp.dto.CoinDTO;
 import com.mn.cryptoapp.entity.Coin;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 @EnableAutoConfiguration
@@ -11,15 +17,19 @@ public class CoinRepository {
 
     private static String INSERT = "insert into coin (name, price, quantity, datetime) values (?,?,?,?)";
 
+    private static String SELECT_ALL = "select name, sum(quantity) as quantity from coin group by name";
+
+    private static String SELECT_BY_NAME = "select * from coin where name = ?";
+
     private JdbcTemplate jdbcTemplate;
 
     public CoinRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Coin insert (Coin coin){
+    public Coin insert(Coin coin) {
 
-        Object[] attr = new Object[] {
+        Object[] attr = new Object[]{
                 coin.getName(),
                 coin.getPrice(),
                 coin.getQuantity(),
@@ -28,5 +38,33 @@ public class CoinRepository {
 
         jdbcTemplate.update(INSERT, attr);
         return coin;
+    }
+
+    public List<CoinDTO> getAll() {
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<CoinDTO>() {
+            @Override
+            public CoinDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+                CoinDTO coin = new CoinDTO();
+                coin.setName(rs.getString("name"));
+                coin.setQuantity(rs.getBigDecimal("quantity"));
+                return coin;
+            }
+        });
+    }
+
+    public List<Coin> getByName(String name) {
+        Object[] attr = new Object[]{name};
+        return jdbcTemplate.query(SELECT_BY_NAME, new RowMapper<Coin>() {
+            @Override
+            public Coin mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Coin coin = new Coin();
+                coin.setId(rs.getInt("id"));
+                coin.setName(rs.getString("name"));
+                coin.setPrice(rs.getBigDecimal("price"));
+                coin.setQuantity(rs.getBigDecimal("quantity"));
+                coin.setDateTime(rs.getTimestamp("datetime"));
+                return coin;
+            }
+        }, attr);
     }
 }
